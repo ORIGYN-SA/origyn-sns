baaf9808c965819120b7f5ee5785bc5afe7f5da1
Dustin Becker <dustin@bochslerfinance.com>
2024-04-04 22:42:49 +0200

    cleanup

--- backend/libraries/ledger_utils/src/lib.rs
+++ backend/libraries/ledger_utils/src/lib.rs
@@ -1,30 +1,18 @@
 use candid::Principal;
-use ic_ledger_types::{ AccountIdentifier, Subaccount, DEFAULT_SUBACCOUNT };
+use ic_ledger_types::{AccountIdentifier, Subaccount, DEFAULT_SUBACCOUNT};
 use icrc_ledger_types::icrc1::account::Account;
-use sha2::{ Digest, Sha256 };
-
-pub fn compute_neuron_staking_subaccount_bytes(controller: Principal, nonce: u64) -> [u8; 32] {
-    const DOMAIN: &[u8] = b"neuron-stake";
-    const DOMAIN_LENGTH: [u8; 1] = [0x0c];
-
-    let mut hasher = Sha256::new();
-    hasher.update(DOMAIN_LENGTH);
-    hasher.update(DOMAIN);
-    hasher.update(controller.as_slice());
-    hasher.update(nonce.to_be_bytes());
-    hasher.finalize().into()
-}
+use sha2::{Digest, Sha256};
 
 pub fn icrc_account_to_legacy_account_id(icrc_account: Account) -> AccountIdentifier {
-    let subaccount: Subaccount = icrc_account.subaccount.map_or(DEFAULT_SUBACCOUNT, |s|
-        Subaccount(s)
-    );
+    let subaccount: Subaccount = icrc_account
+        .subaccount
+        .map_or(DEFAULT_SUBACCOUNT, |s| Subaccount(s));
     AccountIdentifier::new(&icrc_account.owner, &subaccount)
 }
 
 pub fn principal_to_legacy_account_id(
     principal: Principal,
-    subaccount: Option<Subaccount>
+    subaccount: Option<Subaccount>,
 ) -> AccountIdentifier {
     AccountIdentifier::new(&principal, &subaccount.unwrap_or(DEFAULT_SUBACCOUNT))
 }
@@ -40,8 +28,9 @@ mod tests {
     fn convert_icrc_account_to_legacy_account_id() {
         let icrc_account = Account {
             owner: Principal::from_text(
-                "465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae"
-            ).unwrap(),
+                "465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae",
+            )
+            .unwrap(),
             subaccount: None,
         };
         let result = icrc_account_to_legacy_account_id(icrc_account);
@@ -56,8 +45,9 @@ mod tests {
     fn convert_icrc_account_to_legacy_account_id_with_subaccount() {
         let icrc_account = Account {
             owner: Principal::from_text(
-                "465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae"
-            ).unwrap(),
+                "465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae",
+            )
+            .unwrap(),
             subaccount: Some([
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 1,
