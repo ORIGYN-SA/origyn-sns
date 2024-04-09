14f19af0fd1b7efd6e8463b8c3fa7498bed23530
Dustin Becker <dustin@bochslerfinance.com>
2024-04-09 14:20:42 +0200

    minor fix

--- backend/canisters/token_metrics/impl/src/queries/get_neurons_stats.rs
+++ backend/canisters/token_metrics/impl/src/queries/get_neurons_stats.rs
@@ -6,10 +6,10 @@ use crate::state::{read_state, GovernanceStats};
 #[query]
 fn get_neurons_stats(principal: Option<Principal>) -> GovernanceStats {
     if let Some(prin) = principal {
-        let stats_by_principal = read_state(|state| state.data.principal_gov_stats);
+        let stats_by_principal = read_state(|state| state.data.principal_gov_stats.clone());
         return stats_by_principal
             .get(&prin)
-            .unwrap_or_else(|| &GovernanceStats::default())
+            .unwrap_or(&GovernanceStats::default())
             .clone();
     } else {
         read_state(|state| state.data.all_gov_stats)
