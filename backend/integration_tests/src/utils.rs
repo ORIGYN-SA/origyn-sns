3445dc173733584946708d27de00af9ede04e3ec
Dustin Becker <dustin@bochslerfinance.com>
2024-04-15 21:42:54 +0200

    correct random_principal

--- backend/integration_tests/src/utils.rs
+++ backend/integration_tests/src/utils.rs
@@ -2,7 +2,7 @@ use candid::Principal;
 use rand::{ RngCore, thread_rng };
 
 pub fn random_principal() -> Principal {
-    let mut bytes = [0u8; 8];
+    let mut bytes = [0u8; 29];
     thread_rng().fill_bytes(&mut bytes);
     Principal::from_slice(&bytes)
 }
