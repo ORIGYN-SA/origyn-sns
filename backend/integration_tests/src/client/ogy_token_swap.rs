3646070858ee2d4122bbcaf5b5950167cb6c7c4b
Dustin Becker <dustin@bochslerfinance.com>
2024-04-15 21:15:10 +0200

    successful ogy token swap integrstion tests

--- backend/integration_tests/src/client/ogy_token_swap.rs
+++ backend/integration_tests/src/client/ogy_token_swap.rs
@@ -1,1 +1,71 @@
-pub mod swap_tokens {}
+use crate::{ generate_query_call, generate_update_call };
+
+// Queries
+generate_query_call!(request_deposit_account);
+
+// Updates
+generate_update_call!(swap_tokens);
+
+pub mod swap_tokens {
+    use ogy_token_swap::updates::swap_tokens::{ SwapTokensRequest, SwapTokensResponse };
+
+    pub type Args = SwapTokensRequest;
+    pub type Response = SwapTokensResponse;
+}
+
+pub mod request_deposit_account {
+    use candid::Principal;
+    use ic_ledger_types::AccountIdentifier;
+
+    pub type Args = Principal;
+    pub type Response = AccountIdentifier;
+}
+
+pub mod happy_path {
+    use super::*;
+    use candid::Principal;
+    use ic_ledger_types::BlockIndex;
+    use pocket_ic::PocketIc;
+    use types::CanisterId;
+
+    pub fn swap_tokens_authenticated_call(
+        pic: &mut PocketIc,
+        sender: Principal,
+        ogy_token_swap_canister_id: CanisterId,
+        block_index: BlockIndex
+    ) -> swap_tokens::Response {
+        swap_tokens(
+            pic,
+            sender,
+            ogy_token_swap_canister_id,
+            &(swap_tokens::Args {
+                block_index,
+                user: None,
+            })
+        )
+    }
+    pub fn swap_tokens_anonymous_call(
+        pic: &mut PocketIc,
+        ogy_token_swap_canister_id: CanisterId,
+        user: Principal,
+        block_index: BlockIndex
+    ) -> swap_tokens::Response {
+        swap_tokens(
+            pic,
+            Principal::anonymous(),
+            ogy_token_swap_canister_id,
+            &(swap_tokens::Args {
+                block_index,
+                user: Some(user),
+            })
+        )
+    }
+
+    pub fn deposit_account(
+        pic: &PocketIc,
+        ogy_token_swap_canister_id: CanisterId,
+        user: Principal
+    ) -> request_deposit_account::Response {
+        request_deposit_account(pic, Principal::anonymous(), ogy_token_swap_canister_id, &user)
+    }
+}
--- backend/integration_tests/src/tests/ogy_swap.rs
+++ backend/integration_tests/src/tests/ogy_swap.rs
@@ -1,6 +1,85 @@
-use crate::init::init;
+use ic_ledger_types::Subaccount;
+use ledger_utils::principal_to_legacy_account_id;
+use ogy_token_swap::updates::swap_tokens::SwapTokensResponse;
+use utils::consts::{ E8S_FEE_OGY, E8S_PER_OGY };
+
+use crate::{
+    client::{
+        icrc1::happy_path::{ balance_of, transfer },
+        ogy_legacy_ledger::happy_path::{ mint_ogy, transfer_ogy },
+        ogy_token_swap::happy_path::{ deposit_account, swap_tokens_authenticated_call },
+    },
+    init::init,
+    utils::random_principal,
+    TestEnv,
+};
 
 #[test]
 fn valid_swap() {
     let env = init();
+    let TestEnv { mut pic, canister_ids, controller } = env;
+
+    let ogy_legacy_ledger_canister = canister_ids.ogy_legacy_ledger;
+    let ogy_new_ledger_canister = canister_ids.ogy_new_ledger;
+    let ogy_token_swap_canister_id = canister_ids.ogy_swap;
+
+    let ogy_new_ledger_minting_account = controller;
+
+    let user = random_principal();
+    let amount = 100_000 * E8S_PER_OGY;
+
+    // mint tokens to swapping user
+    let _ = mint_ogy(
+        &mut pic,
+        ogy_legacy_ledger_canister,
+        principal_to_legacy_account_id(user, None),
+        amount
+    ).unwrap();
+    // mint tokens to swap reserve pool of swap canister
+    let swap_pool_amount = 9_400_000_000 * E8S_PER_OGY;
+    let _ = transfer(
+        &mut pic,
+        ogy_new_ledger_minting_account,
+        ogy_new_ledger_canister,
+        ogy_token_swap_canister_id,
+        swap_pool_amount.into()
+    );
+
+    let swap_amount = amount - E8S_FEE_OGY;
+
+    let deposit_address = deposit_account(&pic, ogy_token_swap_canister_id, user);
+
+    let block_index_deposit = transfer_ogy(
+        &mut pic,
+        user,
+        ogy_legacy_ledger_canister,
+        deposit_address,
+        swap_amount
+    ).unwrap();
+
+    let result = swap_tokens_authenticated_call(
+        &mut pic,
+        user,
+        ogy_token_swap_canister_id,
+        block_index_deposit
+    );
+
+    assert_eq!(result, SwapTokensResponse::Success);
+
+    assert_eq!(balance_of(&pic, ogy_new_ledger_canister, user), swap_amount)
+}
+
+#[test]
+fn test_deposit_account() {
+    let env = init();
+    let TestEnv { pic, canister_ids, .. } = env;
+
+    let ogy_token_swap_canister_id = canister_ids.ogy_swap;
+
+    let user = random_principal();
+
+    assert_eq!(
+        deposit_account(&pic, ogy_token_swap_canister_id, user),
+        principal_to_legacy_account_id(ogy_token_swap_canister_id, Some(Subaccount::from(user)))
+    )
 }
