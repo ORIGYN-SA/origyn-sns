1fe4c4b071292109a04357263c62d6d057a32214
Dustin Becker <dustin@bochslerfinance.com>
2024-04-16 12:17:05 +0200

    add test for anonymous request

--- backend/integration_tests/src/tests/ogy_token_swap.rs
+++ backend/integration_tests/src/tests/ogy_token_swap.rs
@@ -11,7 +11,12 @@ use crate::{
     client::{
         icrc1::happy_path::{ balance_of, transfer },
         ogy_legacy_ledger::happy_path::{ mint_ogy, transfer_ogy },
-        ogy_token_swap::happy_path::{ deposit_account, swap_info, swap_tokens_authenticated_call },
+        ogy_token_swap::happy_path::{
+            deposit_account,
+            swap_info,
+            swap_tokens_anonymous_call,
+            swap_tokens_authenticated_call,
+        },
     },
     init::init,
     utils::random_principal,
@@ -192,6 +197,71 @@ fn invalid_deposit_account() {
     )
 }
 
+#[test]
+fn test_anonymous_request() {
+    let env = init();
+    let TestEnv { mut pic, canister_ids, controller } = env;
+
+    let ogy_legacy_ledger_canister = canister_ids.ogy_legacy_ledger;
+    let ogy_new_ledger_canister = canister_ids.ogy_new_ledger;
+    let ogy_token_swap_canister_id = canister_ids.ogy_swap;
+
+    let ogy_new_ledger_minting_account = controller;
+
+    let user = random_principal();
+    let amount = 100_000 * E8S_PER_OGY;
+
+    // mint tokens to swapping user
+    let _ = mint_ogy(
+        &mut pic,
+        ogy_legacy_ledger_canister,
+        principal_to_legacy_account_id(user, None),
+        amount
+    ).unwrap();
+    // mint tokens to swap reserve pool of swap canister
+    let swap_pool_amount = 9_400_000_000 * E8S_PER_OGY;
+    let _ = transfer(
+        &mut pic,
+        ogy_new_ledger_minting_account,
+        ogy_new_ledger_canister,
+        ogy_token_swap_canister_id,
+        swap_pool_amount.into()
+    );
+
+    let swap_amount = amount - E8S_FEE_OGY;
+
+    let deposit_address = deposit_account(&pic, ogy_token_swap_canister_id, user);
+
+    let block_index_deposit = transfer_ogy(
+        &mut pic,
+        user,
+        ogy_legacy_ledger_canister,
+        deposit_address,
+        swap_amount
+    ).unwrap();
+
+    // requesting swap with anonymous principal simulating a call from e.g. the team on behalf of the user
+    let result = swap_tokens_anonymous_call(
+        &mut pic,
+        ogy_token_swap_canister_id,
+        user,
+        block_index_deposit
+    );
+
+    assert_eq!(result, SwapTokensResponse::Success);
+
+    assert_eq!(balance_of(&pic, ogy_new_ledger_canister, user), Nat::from(swap_amount));
+
+    assert_eq!(
+        swap_info(
+            &pic,
+            controller,
+            ogy_token_swap_canister_id,
+            block_index_deposit
+        ).unwrap().status,
+        SwapStatus::Complete
+    );
+}
 #[test]
 fn test_deposit_account() {
     let env = init();
