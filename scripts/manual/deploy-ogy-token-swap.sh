29acc9347db8d44e5488d91cb7b832a961f415b4
Dustin Becker <dustin@bochslerfinance.com>
2024-04-25 16:50:45 +0200

    updated testing scripts

--- scripts/manual/deploy-ogy-token-swap.sh
+++ scripts/manual/deploy-ogy-token-swap.sh
@@ -6,7 +6,8 @@
 
 dfx deploy --network staging ogy_token_swap --argument "(record {
   test_mode = true;
-  ogy_legacy_ledger_canister_id = principal \"$(dfx canister id ogy_ledger --network staging)\";
-  ogy_new_ledger_canister_id = principal \"$(dfx canister id ogy_legacy_ledger --network staging)\"
+  ogy_legacy_ledger_canister_id = principal \"$(dfx canister id ogy_legacy_ledger --network staging)\";
+  ogy_new_ledger_canister_id = principal \"$(dfx canister id ogy_ledger --network staging)\";
+  ogy_legacy_minting_account_principal = principal \"$(dfx identity get-principal)\";
   } )" --mode reinstall
 # dfx deploy --network staging ogy_token_swap --argument '(null)'
