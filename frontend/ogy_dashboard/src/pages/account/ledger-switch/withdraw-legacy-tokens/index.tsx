976fd7e80ea03b72168ac322f23410340de7c80e
aditsch <audran@daolink.ch>
2024-04-25 16:58:45 +0200

    fix step swap tokens and integrate new canisters

--- frontend/ogy_dashboard/src/pages/account/ledger-switch/withdraw-legacy-tokens/index.tsx
+++ frontend/ogy_dashboard/src/pages/account/ledger-switch/withdraw-legacy-tokens/index.tsx
@@ -1,34 +1,16 @@
-import { useState } from "react";
-import useConnect from "@helpers/useConnect";
-import { Button, Dialog } from "@components/ui";
+import { Link } from "react-router-dom";
+import { LEGACY_OGY_DASHBOARD_URL } from "@constants/index";
+import { Button } from "@components/ui";
 
 const WithdrawLegacyTokens = () => {
-  const { principalShort } = useConnect();
-  const [show, setShow] = useState(false);
-
-  const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false);
   return (
-    <>
-      <Button className="w-full" onClick={handleShow}>
-        Withdraw your tokens
-      </Button>
-      <Dialog show={show} handleClose={handleClose}>
-        <div className="p-12 text-center">
-          <div className="">
-            <span>You are about to withdraw </span>
-            <span className="font-bold text-lg">?? OGY.</span>
-          </div>
-          <div className="text-sm mb-8">
-            <span className="text-content/60">
-              The tokens will be sent to your principal
-            </span>
-            <span> {principalShort}</span>
-          </div>
-          <Button onClick={handleClose}>Confirm</Button>
-        </div>
-      </Dialog>
-    </>
+    <Link
+      to={LEGACY_OGY_DASHBOARD_URL}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Button className="w-full">Go to Legacy OGY dashboard</Button>
+    </Link>
   );
 };
 
