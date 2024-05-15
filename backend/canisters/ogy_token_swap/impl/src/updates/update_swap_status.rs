9c066efed57f05c418bda3407557a4aa7beff941
Dustin Becker <dustin@bochslerfinance.com>
2024-05-16 00:29:07 +0200

    add method to manipulate swap status -> only for integration testing
    - this method has the feature flag "inttest". It's not compiled by default, only if --inttest is passed as an argument to the build

---
+++ backend/canisters/ogy_token_swap/impl/src/updates/update_swap_status.rs
@@ -0,0 +1,23 @@
+use ic_cdk::update;
+use ic_ledger_types::BlockIndex;
+use ogy_token_swap_api::token_swap::SwapStatus;
+
+pub use ogy_token_swap_api::updates::update_swap_status::{
+    Args as UpdateSwapStateArgs, Response as UpdateSwapStateResponse,
+};
+
+use crate::state::mutate_state;
+
+#[cfg(feature = "inttest")]
+#[update]
+pub async fn update_swap_status(args: UpdateSwapStateArgs) -> UpdateSwapStateResponse {
+    _update_swap_status_impl(args.block_index, args.swap_status).await
+}
+
+async fn _update_swap_status_impl(block_index: BlockIndex, swap_status: SwapStatus) {
+    mutate_state(|s| {
+        s.data
+            .token_swap
+            .update_status(block_index, swap_status.clone())
+    });
+}
