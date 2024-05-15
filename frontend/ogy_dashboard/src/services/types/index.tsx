519d3e9c1c01453e1f54d46aae702d5c1c9e8d93
aditsch <audran@daolink.ch>
2024-05-16 00:11:33 +0200

    add neuron data from result

--- frontend/ogy_dashboard/src/services/types/index.tsx
+++ frontend/ogy_dashboard/src/services/types/index.tsx
@@ -6,10 +6,23 @@ import {
   IProposalVotes,
 } from "@services/types/proposals";
 
+import {
+  INeuronResultAPI,
+  INeuronResultCanister,
+  INeuronData,
+  INeuronDataCanister,
+  IDissolveState,
+} from "@services/types/neurons";
+
 export type {
   IListProps,
   IProposalResult,
   IProposalResults,
   IProposalData,
   IProposalVotes,
+  INeuronResultAPI,
+  INeuronResultCanister,
+  INeuronData,
+  INeuronDataCanister,
+  IDissolveState,
 };
