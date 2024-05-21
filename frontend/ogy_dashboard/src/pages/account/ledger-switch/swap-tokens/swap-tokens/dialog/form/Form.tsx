c8f1f0fefed1ca627d9aa07b9b568277a9a4c3bb
aditsch <audran@daolink.ch>
2024-05-21 13:47:42 +0200

    fix amount to swap in form

--- frontend/ogy_dashboard/src/pages/account/ledger-switch/swap-tokens/swap-tokens/dialog/form/Form.tsx
+++ frontend/ogy_dashboard/src/pages/account/ledger-switch/swap-tokens/swap-tokens/dialog/form/Form.tsx
@@ -1,5 +1,9 @@
+import { useEffect, useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
-import { Button } from "@components/ui";
+import { Tooltip, Skeleton, Button } from "@components/ui";
+import CopyToClipboard from "@components/buttons/CopyToClipboard";
+import { TRANSACTION_FEE } from "@constants/index";
+import { divideBy1e8 } from "@helpers/numbers";
 import { useSwapTokens } from "../../context";
 
 const Form = () => {
@@ -8,7 +12,9 @@ const Form = () => {
     useSwapTokens();
   const { mutate: mutateSendTokens } = sendTokens;
   const { mutate: mutateRequestSwap } = requestSwap;
-  const { data: balanceOGYLegacy } = fetchBalanceLegacy;
+  const { data: balanceOGYLegacy, isSuccess: isSuccessFetchBalanceLegacy } =
+    fetchBalanceLegacy;
+  const [amountSwap, setAmountSwap] = useState<number | null>(null);
 
   const handleOnClick = () => {
     mutateSendTokens(undefined, {
@@ -30,25 +36,55 @@ const Form = () => {
     });
   };
 
+  useEffect(() => {
+    if (isSuccessFetchBalanceLegacy) {
+      setAmountSwap(
+        balanceOGYLegacy.balance - divideBy1e8(Number(TRANSACTION_FEE))
+      );
+    }
+  }, [isSuccessFetchBalanceLegacy, balanceOGYLegacy]);
+
   return (
     <div className="text-center">
       <div className="font-bold text-lg">Swap your OGY to the new ledger</div>
-      <div className="text-sm text-content/60 mb-8">
+      <div className="text-sm text-content/60 mb-12">
         Deposit any OGY tokens that you wish to swap to your account id
       </div>
       <div className="mb-8 rounded-xl bg-surface-2 border border-border">
         <div className="truncate p-4 border-b border-border">
-          Account ID: {accountId}
+          <div className="flex items-center">
+            <div className="mr-2 shrink-0">Account ID: </div>
+            {accountId ? (
+              <>
+                <div
+                  className="truncate"
+                  data-tooltip-id="tooltip_account_id"
+                  data-tooltip-content={accountId}
+                >
+                  {accountId}
+                </div>
+                <Tooltip id="tooltip_account_id" />
+                <CopyToClipboard value={accountId as string} />
+              </>
+            ) : (
+              <Skeleton className="w-32" />
+            )}
+          </div>
         </div>
 
-        <div className="flex justify-between items-center font-bold px-4 py-4">
-          <div className="">Tokens available to swap</div>
-          <div>{balanceOGYLegacy?.balance} OGY</div>
+        <div className="px-4 py-4">
+          <div className="flex justify-between items-center font-bold">
+            <div className="">Tokens available to swap</div>
+            <div>{balanceOGYLegacy?.balance} OGY</div>
+          </div>
+          <div className="flex justify-between items-center text-content/60">
+            <div className="">Transaction fee</div>
+            <div>{divideBy1e8(Number(TRANSACTION_FEE))} OGY</div>
+          </div>
         </div>
       </div>
       <div className="text-content/60 my-8">
-        You will receive {balanceOGYLegacy?.balance} OGY tokens on the new
-        ledger.
+        You will receive {amountSwap} OGY tokens on the new ledger.
       </div>
       <Button onClick={handleOnClick}>
         Swap {balanceOGYLegacy?.balance} OGY tokens
