c7c317c55fbcd993f38e5e62c37eb60dc5e950dd
aditsch <audran@daolink.ch>
2024-05-21 15:50:39 +0200

    update token swap canister

--- frontend/ogy_dashboard/src/services/candid/ogy_token_swap.js
+++ frontend/ogy_dashboard/src/services/candid/ogy_token_swap.js
@@ -1,97 +1,123 @@
 export const idlFactory = ({ IDL }) => {
   const InitArgs = IDL.Record({
-    'ogy_legacy_minting_account_principal' : IDL.Principal,
-    'test_mode' : IDL.Bool,
-    'ogy_new_ledger_canister_id' : IDL.Principal,
-    'ogy_legacy_ledger_canister_id' : IDL.Principal,
+    ogy_legacy_minting_account_principal: IDL.Principal,
+    test_mode: IDL.Bool,
+    authorized_principals: IDL.Vec(IDL.Principal),
+    ogy_new_ledger_canister_id: IDL.Principal,
+    ogy_legacy_ledger_canister_id: IDL.Principal,
   });
+  const Args = IDL.Record({ block_index: IDL.Nat64 });
   const BlockFailReason = IDL.Variant({
-    'ReceiverNotCorrectAccountId' : IDL.Vec(IDL.Nat8),
-    'NotFound' : IDL.Null,
-    'QueryRequestFailed' : IDL.Null,
-    'SenderNotPrincipalDefaultSubaccount' : IDL.Vec(IDL.Nat8),
-    'InvalidOperation' : IDL.Null,
-    'AmountTooSmall' : IDL.Null,
+    ReceiverNotCorrectAccountId: IDL.Vec(IDL.Nat8),
+    NotFound: IDL.Null,
+    QueryRequestFailed: IDL.Null,
+    SenderNotPrincipalDefaultSubaccount: IDL.Vec(IDL.Nat8),
+    InvalidOperation: IDL.Null,
+    AmountTooSmall: IDL.Null,
   });
-  const Tokens = IDL.Record({ 'e8s' : IDL.Nat64 });
+  const Tokens = IDL.Record({ e8s: IDL.Nat64 });
   const TransferError = IDL.Variant({
-    'TxTooOld' : IDL.Record({ 'allowed_window_nanos' : IDL.Nat64 }),
-    'BadFee' : IDL.Record({ 'expected_fee' : Tokens }),
-    'TxDuplicate' : IDL.Record({ 'duplicate_of' : IDL.Nat64 }),
-    'TxCreatedInFuture' : IDL.Null,
-    'InsufficientFunds' : IDL.Record({ 'balance' : Tokens }),
+    TxTooOld: IDL.Record({ allowed_window_nanos: IDL.Nat64 }),
+    BadFee: IDL.Record({ expected_fee: Tokens }),
+    TxDuplicate: IDL.Record({ duplicate_of: IDL.Nat64 }),
+    TxCreatedInFuture: IDL.Null,
+    InsufficientFunds: IDL.Record({ balance: Tokens }),
   });
   const BurnFailReason = IDL.Variant({
-    'TransferError' : TransferError,
-    'CallError' : IDL.Text,
+    TransferError: TransferError,
+    NoTokensToBurn: IDL.Null,
+    CallError: IDL.Text,
   });
   const ImpossibleErrorReason = IDL.Variant({
-    'AmountNotFound' : IDL.Null,
-    'PrincipalNotFound' : IDL.Null,
+    AmountNotFound: IDL.Null,
+    PrincipalNotFound: IDL.Null,
   });
   const TransferError_1 = IDL.Variant({
-    'GenericError' : IDL.Record({
-      'message' : IDL.Text,
-      'error_code' : IDL.Nat,
+    GenericError: IDL.Record({
+      message: IDL.Text,
+      error_code: IDL.Nat,
     }),
-    'TemporarilyUnavailable' : IDL.Null,
-    'BadBurn' : IDL.Record({ 'min_burn_amount' : IDL.Nat }),
-    'Duplicate' : IDL.Record({ 'duplicate_of' : IDL.Nat }),
-    'BadFee' : IDL.Record({ 'expected_fee' : IDL.Nat }),
-    'CreatedInFuture' : IDL.Record({ 'ledger_time' : IDL.Nat64 }),
-    'TooOld' : IDL.Null,
-    'InsufficientFunds' : IDL.Record({ 'balance' : IDL.Nat }),
+    TemporarilyUnavailable: IDL.Null,
+    BadBurn: IDL.Record({ min_burn_amount: IDL.Nat }),
+    Duplicate: IDL.Record({ duplicate_of: IDL.Nat }),
+    BadFee: IDL.Record({ expected_fee: IDL.Nat }),
+    CreatedInFuture: IDL.Record({ ledger_time: IDL.Nat64 }),
+    TooOld: IDL.Null,
+    InsufficientFunds: IDL.Record({ balance: IDL.Nat }),
   });
   const TransferFailReason = IDL.Variant({
-    'TransferError' : TransferError_1,
-    'CallError' : IDL.Text,
+    TransferError: TransferError_1,
+    CallError: IDL.Text,
   });
   const SwapError = IDL.Variant({
-    'BlockFailed' : BlockFailReason,
-    'BurnFailed' : BurnFailReason,
-    'UnexpectedError' : ImpossibleErrorReason,
-    'TransferFailed' : TransferFailReason,
+    BlockFailed: BlockFailReason,
+    BurnFailed: BurnFailReason,
+    UnexpectedError: ImpossibleErrorReason,
+    TransferFailed: TransferFailReason,
+  });
+  const Account = IDL.Record({
+    owner: IDL.Principal,
+    subaccount: IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const TransferRequestArgs = IDL.Record({
+    to: Account,
+    memo: IDL.Opt(IDL.Vec(IDL.Nat8)),
+    created_at_time: IDL.Opt(IDL.Nat64),
+    amount: IDL.Nat,
+  });
+  const Timestamp = IDL.Record({ timestamp_nanos: IDL.Nat64 });
+  const BurnRequestArgs = IDL.Record({
+    memo: IDL.Nat64,
+    from_subaccount: IDL.Opt(IDL.Vec(IDL.Nat8)),
+    created_at_time: IDL.Opt(Timestamp),
+    amount: Tokens,
   });
   const SwapStatus = IDL.Variant({
-    'Failed' : SwapError,
-    'BurnSuccess' : IDL.Null,
-    'Init' : IDL.Null,
-    'Complete' : IDL.Null,
-    'BlockValid' : IDL.Null,
+    Failed: SwapError,
+    BurnSuccess: IDL.Null,
+    Init: IDL.Null,
+    BlockRequest: IDL.Nat64,
+    Complete: IDL.Nat,
+    TransferRequest: TransferRequestArgs,
+    BurnRequest: BurnRequestArgs,
+    BlockValid: IDL.Null,
   });
   const SwapInfo = IDL.Record({
-    'status' : SwapStatus,
-    'principal' : IDL.Principal,
-    'token_swap_block_index' : IDL.Opt(IDL.Nat),
-    'burn_block_index' : IDL.Opt(IDL.Nat64),
-    'timestamp' : IDL.Nat64,
-    'amount' : Tokens,
+    status: SwapStatus,
+    last_request: IDL.Nat64,
+    principal: IDL.Principal,
+    token_swap_block_index: IDL.Opt(IDL.Nat),
+    burn_block_index: IDL.Opt(IDL.Nat64),
+    first_request: IDL.Nat64,
+    amount: IDL.Nat64,
   });
-  const Result = IDL.Variant({ 'Ok' : SwapInfo, 'Err' : IDL.Text });
-  const SwapTokensRequest = IDL.Record({
-    'block_index' : IDL.Nat64,
-    'user' : IDL.Opt(IDL.Principal),
+  const Response = IDL.Variant({
+    Success: SwapInfo,
+    InternalError: IDL.Text,
   });
-  const SwapTokensResponse = IDL.Variant({
-    'Success' : IDL.Null,
-    'InternalError' : IDL.Text,
+  const Args_1 = IDL.Record({ of: IDL.Opt(IDL.Principal) });
+  const Response_1 = IDL.Variant({ Success: IDL.Vec(IDL.Nat8) });
+  const Args_2 = IDL.Record({
+    block_index: IDL.Nat64,
+    user: IDL.Opt(IDL.Principal),
+  });
+  const Response_2 = IDL.Variant({
+    Success: IDL.Nat,
+    InternalError: IDL.Text,
   });
   return IDL.Service({
-    'get_swap_info' : IDL.Func([IDL.Nat64], [Result], ['query']),
-    'request_deposit_account' : IDL.Func(
-        [IDL.Opt(IDL.Principal)],
-        [IDL.Vec(IDL.Nat8)],
-        ['query'],
-      ),
-    'swap_tokens' : IDL.Func([SwapTokensRequest], [SwapTokensResponse], []),
+    get_swap_info: IDL.Func([Args], [Response], ["query"]),
+    request_deposit_account: IDL.Func([Args_1], [Response_1], []),
+    swap_tokens: IDL.Func([Args_2], [Response_2], []),
   });
 };
 export const init = ({ IDL }) => {
   const InitArgs = IDL.Record({
-    'ogy_legacy_minting_account_principal' : IDL.Principal,
-    'test_mode' : IDL.Bool,
-    'ogy_new_ledger_canister_id' : IDL.Principal,
-    'ogy_legacy_ledger_canister_id' : IDL.Principal,
+    ogy_legacy_minting_account_principal: IDL.Principal,
+    test_mode: IDL.Bool,
+    authorized_principals: IDL.Vec(IDL.Principal),
+    ogy_new_ledger_canister_id: IDL.Principal,
+    ogy_legacy_ledger_canister_id: IDL.Principal,
   });
   return [InitArgs];
 };
--- frontend/ogy_dashboard/src/services/queries/switch-ledger/useSendTokens.tsx
+++ frontend/ogy_dashboard/src/services/queries/switch-ledger/useSendTokens.tsx
@@ -4,6 +4,7 @@ import { Principal } from "@dfinity/principal";
 import { AccountIdentifier } from "@dfinity/ledger-icp";
 import { ActorSubclass } from "@dfinity/agent";
 import { TRANSACTION_FEE } from "@constants/index";
+import { Response_1 } from "@services/types/ogy_token_swap";
 
 // const isRejected = (
 //   input: PromiseSettledResult<unknown>
@@ -36,11 +37,15 @@ const sendTokens = async ({
     ledgerLegacyActor.account_balance_dfx({
       account: userAccountIdentifier.toHex(),
     }),
-    OGYTokenSwapActor.request_deposit_account([Principal.fromText(owner)]),
+    OGYTokenSwapActor.request_deposit_account({
+      of: [Principal.fromText(owner)],
+    }),
   ]);
 
   const result = rawResult.filter(isFulfilled);
-  const to = Principal.fromUint8Array(result[1]?.value as Uint8Array).toHex();
+  const to = Principal.fromUint8Array(
+    (result[1]?.value as Response_1).Success as Uint8Array
+  ).toHex();
   const fee = { e8s: TRANSACTION_FEE };
   const amount = (result[0]?.value as IValueOGYBalance).e8s - TRANSACTION_FEE;
 
