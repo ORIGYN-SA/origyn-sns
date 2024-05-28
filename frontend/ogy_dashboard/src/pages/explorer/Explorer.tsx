f5383bc53981731f8d5691eff41b628ca21cb98e
aditsch <audran@daolink.ch>
2024-05-28 10:23:27 +0200

    Remove search for v1

--- frontend/ogy_dashboard/src/pages/explorer/Explorer.tsx
+++ frontend/ogy_dashboard/src/pages/explorer/Explorer.tsx
@@ -1,9 +1,9 @@
-import { useMemo } from "react";
+// import { useMemo } from "react";
 // import { useLoaderData, defer, Await } from "react-router-dom";
 import { usePagination, useSorting } from "@helpers/table/useTable";
 import TransactionsList from "@pages/transactions/transactions-list/TransactionsList";
-import Badge from "@components/ui/Badge";
-import { Search } from "@components/ui";
+// import Badge from "@components/ui/Badge";
+// import { Search } from "@components/ui";
 
 const loader = async () => {
   return null;
@@ -16,24 +16,24 @@ export const Explorer = () => {
     id: "index",
     desc: true,
   });
-  const searchForItems = useMemo(
-    () => [
-      { title: "PrincipalID", bgColorCn: "bg-jade/20", colorCn: "text-jade" },
-      {
-        title: "Block index",
-        bgColorCn: "bg-candyFloss/20",
-        colorCn: "text-candyFloss",
-      },
-    ],
-    []
-  );
+  // const searchForItems = useMemo(
+  //   () => [
+  //     { title: "PrincipalID", bgColorCn: "bg-jade/20", colorCn: "text-jade" },
+  //     {
+  //       title: "Block index",
+  //       bgColorCn: "bg-candyFloss/20",
+  //       colorCn: "text-candyFloss",
+  //     },
+  //   ],
+  //   []
+  // );
 
   return (
     <div className="container mx-auto py-16 px-4">
       <div className="flex flex-col items-center">
         <div className="max-w-4xl text-center">
           <h1 className="text-4xl sm:text-6xl font-bold">Explorer</h1>
-          <div className="flex gap-2 mt-8">
+          {/* <div className="flex gap-2 mt-8">
             <div>Search for: </div>
             {searchForItems.map(({ title, bgColorCn, colorCn }, index) => {
               return (
@@ -46,11 +46,11 @@ export const Explorer = () => {
                 </div>
               );
             })}
-          </div>
+          </div> */}
         </div>
       </div>
 
-      <Search id="search-explorer" className="max-w-2xl m-auto mt-8" />
+      {/* <Search id="search-explorer" className="max-w-2xl m-auto mt-8" /> */}
       <div className="mt-16">
         <TransactionsList
           pagination={pagination}
