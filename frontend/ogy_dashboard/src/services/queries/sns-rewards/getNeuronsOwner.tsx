4f6ba1b6adb8ecdd25392617aa562f8168ae3567
aditsch <audran@daolink.ch>
2024-05-30 18:47:58 +0200

    fix neurons owner list

---
+++ frontend/ogy_dashboard/src/services/queries/sns-rewards/getNeuronsOwner.tsx
@@ -0,0 +1,18 @@
+import { ActorSubclass } from "@dfinity/agent";
+
+const getNeuronsByOwner = async ({
+  snsRewardsActor,
+}: {
+  snsRewardsActor: ActorSubclass;
+}): Promise<string[]> => {
+  const result = (await snsRewardsActor.get_neurons_by_owner()) as Array<
+    { id: number[] }[]
+  >;
+  return result && result[0]
+    ? result[0].map((neuron) => {
+        return Buffer.from(neuron.id).toString("hex");
+      })
+    : [];
+};
+
+export default getNeuronsByOwner;
