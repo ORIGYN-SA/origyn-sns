50030a7de3cd94e031ad1c1720ce4e207ce4a53d
aditsch <audran@daolink.ch>
2024-06-06 10:29:49 +0200

    test with console.log

--- frontend/ogy_dashboard/src/pages/account/available-ogy/index.tsx
+++ frontend/ogy_dashboard/src/pages/account/available-ogy/index.tsx
@@ -19,6 +19,7 @@ const AvailableOGY = () => {
   const { data: balanceOGYUSD } = useFetchBalanceOGYUSD({
     balance: balanceOGY?.balance,
   });
+  console.log("test husky");
 
   return (
     <Card>
