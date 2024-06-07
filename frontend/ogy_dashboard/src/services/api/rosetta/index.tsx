c0e3ae3985c411589105d23e4966018240702722
aditsch <audran@daolink.ch>
2024-06-07 10:21:27 +0200

    add timeout

--- frontend/ogy_dashboard/src/services/api/rosetta/index.tsx
+++ frontend/ogy_dashboard/src/services/api/rosetta/index.tsx
@@ -4,6 +4,7 @@ import { API_ROSETTA } from "@constants/index";
 const instance = axios.create({
   baseURL: API_ROSETTA,
   withCredentials: false,
+  timeout: 5000,
 });
 
 export default instance;
