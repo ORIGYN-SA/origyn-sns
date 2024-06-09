c4ba595b8cea28fa0fbff1948d7b66b828abfd0c
Sebastian Deme <sebastian@origyn.ch>
2024-06-10 01:13:30 +0300

    chore: latest working

--- backend/integration_tests/src/daily_jobs_suite/tests/daily_jobs.rs
+++ backend/integration_tests/src/daily_jobs_suite/tests/daily_jobs.rs
@@ -5,34 +5,33 @@ use utils::consts::E8S_PER_OGY;
 use crate::client::icrc1::client::{ balance_of, transfer, total_supply };
 use crate::daily_jobs_suite::{ init::init, TestEnv };
 
-use crate::utils::random_principal;
-
 #[test]
 fn daily_burn_job() {
     let env = init();
     let TestEnv { mut pic, canister_ids, controller } = env;
 
     let ledger_canister_id = canister_ids.ogy_ledger_canister_id;
+    let daily_jobs_canister_id = canister_ids.daily_jobs_canister_id;
 
     let minting_account = controller;
 
-    let principal1 = Account { owner: random_principal(), subaccount: None };
+    let daily_jobs_account = Account { owner: daily_jobs_canister_id, subaccount: None };
 
-    // Make the initial mint transaction to principal1
+    // Make the initial mint transaction to daily_jobs_account
     assert_eq!(
         transfer(
             &mut pic,
             minting_account,
             ledger_canister_id,
             None,
-            principal1,
+            daily_jobs_account,
             (100_000_000 * E8S_PER_OGY).into()
         ),
         Ok((0u8).into())
     );
 
     assert_eq!(
-        balance_of(&pic, ledger_canister_id, principal1),
+        balance_of(&pic, ledger_canister_id, daily_jobs_account),
         Nat::from(100_000_000 * E8S_PER_OGY)
     );
 
@@ -46,10 +45,23 @@ fn daily_burn_job() {
         // Advance one day to allow the burn transaction to take place
         pic.advance_time(Duration::from_secs(86_400));
 
+        // For some reason we need this
+        for _ in 1..20 {
+            pic.tick();
+        }
+
         // Check the total supply to be 90_000_000 OGY, the job is burning 1_000_000 OGY
         let burnt_so_far = i * 1_000_000;
         let remaining_total_supply = 100_000_000 - burnt_so_far;
         println!("Remaining in day {} is: {}", i, remaining_total_supply);
+
+        // Expect the balance of principal 1 to be less with 1_000_000 (how much was burnt)
+        assert_eq!(
+            balance_of(&pic, ledger_canister_id, daily_jobs_account),
+            Nat::from(remaining_total_supply * E8S_PER_OGY)
+        );
+
+        // Also expect the total supply to be less with 1_000_000
         assert_eq!(
             total_supply(&pic, ledger_canister_id),
             Nat::from(remaining_total_supply * E8S_PER_OGY)
