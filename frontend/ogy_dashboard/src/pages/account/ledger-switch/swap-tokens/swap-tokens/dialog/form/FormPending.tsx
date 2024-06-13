8463a1755df5cb766df3be1e80f851aad3117cc2
aditsch <audran@daolink.ch>
2024-06-13 16:21:40 +0200

    update orange color

--- frontend/ogy_dashboard/src/pages/account/ledger-switch/swap-tokens/swap-tokens/dialog/form/FormPending.tsx
+++ frontend/ogy_dashboard/src/pages/account/ledger-switch/swap-tokens/swap-tokens/dialog/form/FormPending.tsx
@@ -3,7 +3,7 @@ import { LoaderSpin } from "@components/ui";
 const FormPending = () => {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-full bg-orange-500 p-4 rounded-xl text-center">
+      <div className="w-full bg-orange-400 p-4 rounded-xl text-center">
         The swapping process can take a little time.
         <br />
         <span className="text-lg font-bold">Do not refresh the page !</span>
