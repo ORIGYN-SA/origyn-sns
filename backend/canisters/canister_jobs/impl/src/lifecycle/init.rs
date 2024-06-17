00c8b4f173348459a3f38015d2bc6f48183a8a77
Sebastian Deme <sebastian@origyn.ch>
2024-06-17 11:25:00 +0000

    Resolve "Daily OGY burn job"

---
+++ backend/canisters/canister_jobs/impl/src/lifecycle/init.rs
@@ -0,0 +1,27 @@
+use ic_cdk_macros::init;
+pub use canister_jobs_api::init::InitArgs;
+use tracing::info;
+use utils::env::CanisterEnv;
+
+use crate::state::{ Data, RuntimeState };
+
+use super::init_canister;
+
+#[init]
+fn init(args: InitArgs) {
+    canister_logger::init(args.test_mode);
+
+    let env = CanisterEnv::new(args.test_mode);
+    let data = Data::new(
+        args.ledger_canister_id,
+        args.burn_principal_id,
+        args.daily_burn_amount,
+        args.authorized_principals
+    );
+
+    let runtime_state = RuntimeState::new(env.clone(), data);
+
+    init_canister(runtime_state);
+
+    info!("Init complete.")
+}
---
+++ backend/canisters/canister_jobs/impl/src/queries/mod.rs
@@ -0,0 +1,5 @@
+pub mod get_burn_history;
+pub mod set_daily_burn_amount_validate;
+
+pub use get_burn_history::*;
+pub use set_daily_burn_amount_validate::*;
