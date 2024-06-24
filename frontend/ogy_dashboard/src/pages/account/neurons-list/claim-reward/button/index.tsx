9e34679b64d7b023635e7aa58b2d0adcb4f8a86a
aditsch <audran@daolink.ch>
2024-06-24 15:46:03 +0200

    fix user can claim even if 0

--- frontend/ogy_dashboard/src/pages/account/neurons-list/claim-reward/button/index.tsx
+++ frontend/ogy_dashboard/src/pages/account/neurons-list/claim-reward/button/index.tsx
@@ -3,7 +3,11 @@ import { useClaimReward } from "../context";
 
 const BtnClaimReward = () => {
   const { handleShow, claimAmount } = useClaimReward();
-  return <Button onClick={handleShow}>Claim {claimAmount} OGY</Button>;
+  return (
+    <Button onClick={handleShow} disabled={claimAmount === 0}>
+      Claim {claimAmount} OGY
+    </Button>
+  );
 };
 
 export default BtnClaimReward;
--- frontend/ogy_dashboard/src/pages/account/staked-rewards/claim-all-rewards/button/index.tsx
+++ frontend/ogy_dashboard/src/pages/account/staked-rewards/claim-all-rewards/button/index.tsx
@@ -2,9 +2,13 @@ import { Button } from "@components/ui";
 import { useClaimAllRewards } from "../context";
 
 const BtnClaimAllRewards = () => {
-  const { handleShow } = useClaimAllRewards();
+  const { handleShow, claimAmount } = useClaimAllRewards();
   return (
-    <Button className="w-full" onClick={handleShow}>
+    <Button
+      className="w-full"
+      onClick={handleShow}
+      disabled={claimAmount === 0}
+    >
       Claim all
     </Button>
   );
