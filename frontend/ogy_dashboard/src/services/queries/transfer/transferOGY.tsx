73280b4da5b6e96885591bcc37c9e3a9386bf03a
Audran Ditsch <audran@daolink.ch>
2024-08-15 10:55:19 +0000

    Resolve "Support account/subaccount address transfer"

--- frontend/ogy_dashboard/src/services/queries/transfer/transferOGY.tsx
+++ frontend/ogy_dashboard/src/services/queries/transfer/transferOGY.tsx
@@ -1,12 +1,17 @@
-import { Principal } from "@dfinity/principal";
 import { TRANSACTION_FEE } from "@constants/index";
 import { getActor } from "@amerej/artemis-react";
+import { decodeIcrcAccount } from "@dfinity/ledger-icrc";
 
 const transferOGY = async ({ amount, to }: { amount: bigint; to: string }) => {
   // https://forum.dfinity.org/t/difference-between-nnsledger-container-icrc1-transfer-and-transfer/20535/5
   const actor = await getActor("ledger", { isAnon: false });
+
+  const decodedAccount = decodeIcrcAccount(to);
+  const owner = decodedAccount.owner;
+  const subaccount = decodedAccount?.subaccount ?? [];
+
   const result = await actor.icrc1_transfer({
-    to: { owner: Principal.fromText(to), subaccount: [] },
+    to: { owner: owner, subaccount: subaccount },
     fee: [],
     memo: [],
     from_subaccount: [],
