5723ba9c8a87a9d85e43ae4c11de35287a98c163
Sebastian Deme <sebastian@origyn.ch>
2024-09-16 01:07:50 +0300

    chore: add candid

--- backend/canisters/nft_index/impl/src/queries/mod.rs
+++ backend/canisters/nft_index/impl/src/queries/mod.rs
@@ -1,5 +1,6 @@
 pub mod get_certificates_by_collection_id;
 pub mod get_certificates_by_category;
+pub mod candid;
 
 pub use get_certificates_by_collection_id::*;
 pub use get_certificates_by_category::*;
