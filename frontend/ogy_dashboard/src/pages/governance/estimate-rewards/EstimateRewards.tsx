a759d5bfa5ce7da1e999bd92e135bad1e3e1a3fb
Victor Raguin <victor.raguin@live.fr>
2024-11-26 17:16:48 +0100

    feat: Add count participant to estimated reward component

--- frontend/ogy_dashboard/src/pages/governance/estimate-rewards/EstimateRewards.tsx
+++ frontend/ogy_dashboard/src/pages/governance/estimate-rewards/EstimateRewards.tsx
@@ -1,115 +1,143 @@
 import Slider, {
-  SliderThumb,
+  SliderThumb
   // SliderValueLabelProps,
-} from "@mui/material/Slider";
-import { Mark } from "@mui/base/useSlider";
-import { styled } from "@mui/material/styles";
-import { Card } from "@components/ui";
-import useEstimatedRewards from "@hooks/governance/useEstimatedRewards";
-import { useState } from "react";
+} from '@mui/material/Slider'
+import { Mark } from '@mui/base/useSlider'
+import { styled } from '@mui/material/styles'
+import { Card } from '@components/ui'
+import useEstimatedRewards from '@hooks/governance/useEstimatedRewards'
+import { useState } from 'react'
 
 interface EstimateRewardsProps {
-  className?: string;
+  className?: string
 }
 
 const AirbnbSlider = styled(Slider)(({ theme }) => ({
-  color: "#3a8589",
+  color: '#3a8589',
   height: 10,
-  padding: "13px 0",
-  "& .MuiSlider-thumb": {
+  padding: '13px 0',
+  '& .MuiSlider-thumb': {
     height: 27,
     width: 27,
-    backgroundColor: "#fff",
-    border: "1px solid currentColor",
-    "&:hover": {
-      boxShadow: "0 0 0 8px rgba(58, 133, 137, 0.16)",
+    backgroundColor: '#fff',
+    border: '1px solid currentColor',
+    '&:hover': {
+      boxShadow: '0 0 0 8px rgba(58, 133, 137, 0.16)'
     },
-    "& .airbnb-bar": {
+    '& .airbnb-bar': {
       height: 9,
       width: 1,
-      backgroundColor: "currentColor",
+      backgroundColor: 'currentColor',
       marginLeft: 1,
-      marginRight: 1,
-    },
+      marginRight: 1
+    }
   },
-  "& .MuiSlider-track": {
-    height: 10,
+  '& .MuiSlider-track': {
+    height: 10
   },
-  "& .MuiSlider-rail": {
-    color: theme.palette.mode === "dark" ? "#bfbfbf" : "#d8d8d8",
-    opacity: theme.palette.mode === "dark" ? undefined : 1,
-    height: 10,
-  },
-}));
+  '& .MuiSlider-rail': {
+    color: theme.palette.mode === 'dark' ? '#bfbfbf' : '#d8d8d8',
+    opacity: theme.palette.mode === 'dark' ? undefined : 1,
+    height: 10
+  }
+}))
 
 interface AirbnbThumbComponentProps extends React.HTMLAttributes<unknown> {}
 
-function AirbnbThumbComponent(props: AirbnbThumbComponentProps) {
-  const { children, ...other } = props;
+function AirbnbThumbComponent (props: AirbnbThumbComponentProps) {
+  const { children, ...other } = props
   return (
     <SliderThumb {...other}>
       {children}
-      <span className="airbnb-bar" />
-      <span className="airbnb-bar" />
-      <span className="airbnb-bar" />
+      <span className='airbnb-bar' />
+      <span className='airbnb-bar' />
+      <span className='airbnb-bar' />
     </SliderThumb>
-  );
+  )
 }
 
 const EstimateRewards = ({ className, ...restProps }: EstimateRewardsProps) => {
-  const { data, isSuccess } = useEstimatedRewards();
-  const [activeIndex, setActiveIndex] = useState(1);
+  const { data, isSuccess } = useEstimatedRewards()
+  const [activeIndex, setActiveIndex] = useState(1)
 
   const handleOnChange = (v: Event) => {
-    setActiveIndex(Number((v.target as HTMLInputElement).value));
-  };
+    setActiveIndex(Number((v.target as HTMLInputElement).value))
+  }
 
   return (
     <Card className={`${className} text-center xl:text-left`} {...restProps}>
-      <h2 className="text-lg font-semibold text-content/60 mb-4">
+      <h2 className='text-lg font-semibold text-content/60 mb-4'>
         Estimate rewards
       </h2>
       {isSuccess && data && (
-        <div className="w-full px-4 py-4">
-          <div className="gap-2">
-            <div className="text-4xl mb-8">{data[activeIndex - 1].rate}</div>
+        <div className='w-full px-2 py-2 md:px-4 md:py-4 xl:px-2 2xl:px-4'>
+          <div className='gap-2'>
+            <div className='text-4xl mb-8'>{data[activeIndex - 1].rate}</div>
           </div>
           <AirbnbSlider
             slots={{ thumb: AirbnbThumbComponent }}
-            aria-label="Restricted values"
+            aria-label='Restricted values'
             value={activeIndex}
-            // getAriaValueText={valuetext}
             step={null}
-            valueLabelDisplay="auto"
+            valueLabelDisplay='auto'
             min={1}
             max={5}
             marks={data as Mark[]}
-            onChange={(v) => handleOnChange(v)}
+            onChange={v => handleOnChange(v)}
           />
-          <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 mt-6">
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-8 mt-6'>
             {activeIndex < 5 && (
-              <div className="flex flex-col items-center xl:items-start">
-                <div className="flex items-center justify-center text-2xl font-semibold">
-                  <img src="/ogy_logo.svg" alt="OGY Logo" />
-                  <span className="ml-2 mr-3">
-                    {data[activeIndex - 1].lockedSum}
-                  </span>
-                  <span className="text-content/60">OGY</span>
+              <div className='flex flex-col items-center xl:items-start'>
+                <div className='flex flex-col md:flex-row xl:flex-col 2xl:flex-row items-center text-2xl font-semibold md:space-x-4 xl:space-x-0 2xl:space-x-4'>
+                  <img
+                    src='/ogy_logo.svg'
+                    alt='OGY Logo'
+                    className='h-14 w-14 object-contain mb-2 md:mb-0 xl:mb-2 2xl:mb-0'
+                  />
+                  <div className='flex flex-col text-left'>
+                    <div className='flex items-baseline'>
+                      <span className='text-2xl'>
+                        {data[activeIndex - 1].lockedSum}
+                      </span>
+                      <span className='ml-2 text-content/60 text-2xl'>OGY</span>
+                    </div>
+                    <div className='text-content/60 text-lg'>
+                      <span className='text-content'>
+                        {data[activeIndex - 1].countSum}
+                      </span>{' '}
+                      participants
+                    </div>
+                  </div>
                 </div>
-                <div className="text-content/60">
+                <div className='text-content/60 mt-2'>
                   currently locked for at least {activeIndex} year
                 </div>
               </div>
             )}
-            <div className="flex flex-col items-center xl:items-start">
-              <div className="flex items-center text-2xl font-semibold">
-                <img src="/ogy_logo.svg" alt="OGY Logo" />
-                <span className="ml-2 mr-3">
-                  {data[activeIndex - 1].locked}
-                </span>
-                <span className="text-content/60">OGY</span>
+
+            <div className='flex flex-col items-center xl:items-start'>
+              <div className='flex flex-col md:flex-row xl:flex-col 2xl:flex-row items-center text-2xl font-semibold md:space-x-4 xl:space-x-0 2xl:space-x-4'>
+                <img
+                  src='/ogy_logo.svg'
+                  alt='OGY Logo'
+                  className='h-14 w-14 object-contain mb-2 md:mb-0 xl:mb-2 2xl:mb-0'
+                />
+                <div className='flex flex-col text-left'>
+                  <div className='flex items-baseline'>
+                    <span className='text-2xl'>
+                      {data[activeIndex - 1].locked}
+                    </span>
+                    <span className='ml-2 text-content/60 text-2xl'>OGY</span>
+                  </div>
+                  <div className='text-content/60 text-lg'>
+                    <span className='text-content'>
+                      {data[activeIndex - 1].count}
+                    </span>{' '}
+                    participants
+                  </div>
+                </div>
               </div>
-              <div className="text-content/60">
+              <div className='text-content/60 mt-2'>
                 currently locked for {activeIndex} year
               </div>
             </div>
@@ -117,7 +145,7 @@ const EstimateRewards = ({ className, ...restProps }: EstimateRewardsProps) => {
         </div>
       )}
     </Card>
-  );
-};
+  )
+}
 
-export default EstimateRewards;
+export default EstimateRewards
