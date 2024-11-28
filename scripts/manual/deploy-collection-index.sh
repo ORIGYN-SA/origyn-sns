a2119d398f0495e741547a8a389e7529a10a2efb
Sebastian Deme <sebastian@origyn.ch>
2024-11-28 17:17:17 +0200

    chore: update manual deploy

--- scripts/manual/deploy-collection-index.sh
+++ scripts/manual/deploy-collection-index.sh
@@ -5,9 +5,9 @@
 ### FOR STAGING
 ###############
 
-./scripts/build-canister.sh collection_index &&
-./scripts/generate-did.sh collection_index &&
-./scripts/build-canister.sh collection_index &&
+# ./scripts/build-canister.sh collection_index &&
+# ./scripts/generate-did.sh collection_index &&
+# ./scripts/build-canister.sh collection_index &&
 # dfx deploy --network staging sns_rewards --argument '(record {test_mode = true})' --mode reinstall
 dfx deploy --network staging collection_index --argument "(record {
     test_mode = true;
@@ -25,28 +25,28 @@ dfx canister call --network staging collection_index insert_category "(record { 
 
 
 # GLD NFT 1g prod
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = true; collection_canister_id = principal \"io7gn-vyaaa-aaaak-qcbiq-cai\"; category = 0; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = true; collection_canister_id = principal \"io7gn-vyaaa-aaaak-qcbiq-cai\"; category = \"Metals\"; })"
 
 # GLD NFT 10g prod
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = true; collection_canister_id = principal \"sy3ra-iqaaa-aaaao-aixda-cai\"; category = 0; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = true; collection_canister_id = principal \"sy3ra-iqaaa-aaaao-aixda-cai\"; category = \"Metals\"; })"
 
 # GLD NFT 1g staging 
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = true; collection_canister_id = principal \"obapm-2iaaa-aaaak-qcgca-cai\"; category = 0; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = true; collection_canister_id = principal \"obapm-2iaaa-aaaak-qcgca-cai\"; category = \"Metals\"; })"
 
 # GLD NFT 10g staging
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"xyo2o-gyaaa-aaaal-qb55a-cai\"; category = 0; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"xyo2o-gyaaa-aaaal-qb55a-cai\"; category = \"Metals\"; })"
 
 # Federitaly classic
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"nszbk-7iaaa-aaaap-abczq-cai\"; category = 2; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"nszbk-7iaaa-aaaap-abczq-cai\"; category = \"Cars\"; })"
 
 # IGI Diamonds
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"lyhlf-uqaaa-aaaap-abj7q-cai\"; category = 1; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"lyhlf-uqaaa-aaaap-abj7q-cai\"; category = \"meme NFTS\"; })"
 
 # Watches
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"4clim-pyaaa-aaaap-abkaa-cai\"; category = 4; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"4clim-pyaaa-aaaap-abkaa-cai\"; category = \"Art\"; })"
 
 # Suzanne Syz collection
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"gq4qu-3iaaa-aaaap-ahnfa-cai\"; category = 1; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"gq4qu-3iaaa-aaaap-ahnfa-cai\"; category = \"meme NFTS\"; })"
 
 # Cyber
-dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"hpcxb-6qaaa-aaaap-ahsea-cai\"; category = 3; })"
+dfx canister call --network staging collection_index insert_collection "(record {is_promoted = false; collection_canister_id = principal \"hpcxb-6qaaa-aaaap-ahsea-cai\"; category = \"Diamonds\"; })"
