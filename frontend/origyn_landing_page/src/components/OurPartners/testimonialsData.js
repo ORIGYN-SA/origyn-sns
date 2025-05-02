c904805e50ac1b192d77206c655e556ed479affc
Sebastian Deme <sebastian@origyn.ch>
2025-05-02 11:36:59 +0300

    chore(landing): update testimonials photo to be compressed

--- frontend/origyn_landing_page/src/components/OurPartners/testimonialsData.js
+++ frontend/origyn_landing_page/src/components/OurPartners/testimonialsData.js
@@ -6,7 +6,7 @@ export const testimonials = [
     title: "Co-Founder of Ciber",
     description:
       '"We always say that diamonds will last forever, which is true. So the same has to be thought about the information you provide because in 100 years, 200 years those diamonds will still exist so we needed something with the technology to make sure we could still provide those informations. Mainly with a product that has a lot of value and goes through generations…The main reason we made a partnership with ORIGYN is because they could provide all of this"',
-    backgroundImage: "/t-gil.png",
+    backgroundImage: "/t-gil-c.jpg",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ export const testimonials = [
     title: "Artist",
     description:
       "\"Inscribing on chain is something beautiful. It's exactly what we are doing with ORIGYN when we do the certification of the physical art piece. We are inscribing on chain something that exist for real… At the end of the day you need a technology that's going to push the boundaries a little bit further, a technology that can certify a physical art piece exactly with the details and with all the information needed to make it part of the blockchain, that's what you do with ORIGYN.\"",
-    backgroundImage: "/t-leo.png",
+    backgroundImage: "/t-leo-c.jpg",
   },
   {
     id: 3,
@@ -24,6 +24,6 @@ export const testimonials = [
     title: "Director of Metalor's Marin site",
     description:
       '"ORIGYN is the most advanced digital certification technology and the most comprehensive in the world. All data is fully stored in a blockchain to ensure that the certificates are immutable and transparent. Metalor is pleased to work with a Neuchâtel-based Fintech company like ORIGYN to bring an innovative solution to the markets."',
-    backgroundImage: "/t-metalor.png",
+    backgroundImage: "/t-metalor-c.jpg",
   },
 ];
