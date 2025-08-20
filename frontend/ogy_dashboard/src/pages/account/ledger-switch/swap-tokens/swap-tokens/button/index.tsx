3c180a71033f692ead7a722269e8e55d5806c5af
Sebastian Deme <sebastian@origyn.ch>
2025-08-20 14:10:06 +0300

    feat(ogy_dashboard): only allow swapping more than 1'000 OGY

--- frontend/ogy_dashboard/src/pages/account/ledger-switch/swap-tokens/swap-tokens/button/index.tsx
+++ frontend/ogy_dashboard/src/pages/account/ledger-switch/swap-tokens/swap-tokens/button/index.tsx
@@ -3,14 +3,29 @@ import { useSwapTokens } from "../context";
 
 const Btn = () => {
   const { handleShow, fetchBalanceLegacy, accountId } = useSwapTokens();
+  const { data: balanceOGYLegacy } = fetchBalanceLegacy;
+
+  const needsMoreForSwap =
+    typeof balanceOGYLegacy?.balance === "undefined" ||
+    balanceOGYLegacy?.balance < 1_000 * 1e8;
+
   return (
-    <Button
-      className="w-full"
-      onClick={handleShow}
-      disabled={!fetchBalanceLegacy.isSuccess && !accountId}
-    >
-      Swap your tokens
-    </Button>
+    <>
+      <Button
+        className="w-full"
+        onClick={handleShow}
+        disabled={
+          (!fetchBalanceLegacy.isSuccess && !accountId) || needsMoreForSwap
+        }
+      >
+        Swap your tokens
+      </Button>
+      {needsMoreForSwap && (
+        <div className="text-sm text-red-500 text-center mt-2">
+          You need to have at least 1'000 OGY to swap
+        </div>
+      )}
+    </>
   );
 };
 
