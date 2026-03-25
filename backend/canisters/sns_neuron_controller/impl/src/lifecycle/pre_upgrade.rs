df4757a2cfbad30051222ccc497f03922f175b52
Victoria <victoria.horbunova@bity.com>
2026-03-25 16:35:30 +0100

    fix: remove serializer

--- backend/canisters/sns_neuron_controller/impl/src/lifecycle/pre_upgrade.rs
+++ backend/canisters/sns_neuron_controller/impl/src/lifecycle/pre_upgrade.rs
@@ -20,5 +20,5 @@ fn pre_upgrade() {
     let mut memory = get_upgrades_memory();
     let writer = get_writer(&mut memory);
 
-    serializer::serialize(stable_state, writer).unwrap();
+    bity_ic_serializer::serialize(stable_state, writer).unwrap();
 }
