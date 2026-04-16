b0fbd1328071d8edfd12a029c51e145f5dab1574
Giovanni Gigantino <giovanni.gigantino@bity.com>
2026-04-16 15:53:18 +0200

    fix(dashboard): remove Transaction History from navbar

--- frontend/ogy_dashboard/src/components/navbar/Navbar.tsx
+++ frontend/ogy_dashboard/src/components/navbar/Navbar.tsx
@@ -13,7 +13,6 @@ import useScrolledPast from "@hooks/useScrolledPast";
 const navItems: { title: string; url: string; requiresAuth?: boolean }[] = [
   { title: "Dashboard", url: "/" },
   { title: "Governance", url: "/governance" },
-  { title: "Transaction History", url: "/explorer" },
   { title: "Proposals", url: "/proposals" },
   { title: "Calculator", url: "/calculator" },
   { title: "Certificates", url: "/certificates" },
