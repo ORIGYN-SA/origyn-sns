84e22bdf13bd9931c187d8cb9953da764155ada2
Giovanni Gigantino <giovanni.gigantino@bity.com>
2026-05-21 16:54:55 +0200

    fix: downgrade tailwind to fix build

--- frontend/origyn_landing_page/vite.config.js
+++ frontend/origyn_landing_page/vite.config.js
@@ -3,12 +3,11 @@ import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import tailwindcss from "@tailwindcss/vite";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
