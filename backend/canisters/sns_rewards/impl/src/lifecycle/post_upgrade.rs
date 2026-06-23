ed390df2c8376a1df7683c107e7c8b72588baa85
Victoria <victoria.horbunova@bity.com>
2026-06-23 15:01:27 +0300

    fix: integration tests + comment out migration

--- backend/canisters/sns_rewards/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/sns_rewards/impl/src/lifecycle/post_upgrade.rs
@@ -24,17 +24,17 @@ fn post_upgrade(args: Args) {
             let reader = get_reader(&memory);
 
             // uncomment these lines if you want to do a normal upgrade
-            // let (mut state, logs, traces): (RuntimeState, Vec<LogEntry>, Vec<LogEntry>) = serializer
-            //     ::deserialize(reader)
-            //     .unwrap();
+            let (mut state, logs, traces): (RuntimeState, Vec<LogEntry>, Vec<LogEntry>) = bity_ic_serializer
+                ::deserialize(reader)
+                .unwrap();
 
             // uncomment these lines if you want to do an upgrade with migration
-            let (runtime_state_v0, logs, traces): (
-                RuntimeStateV0,
-                Vec<LogEntry>,
-                Vec<LogEntry>,
-            ) = bity_ic_serializer::deserialize(reader).unwrap();
-            let mut state = RuntimeState::from(runtime_state_v0);
+            // let (runtime_state_v0, logs, traces): (
+            //     RuntimeStateV0,
+            //     Vec<LogEntry>,
+            //     Vec<LogEntry>,
+            // ) = bity_ic_serializer::deserialize(reader).unwrap();
+            // let mut state = RuntimeState::from(runtime_state_v0);
 
             state.env.set_version(upgrade_args.version);
             state.env.set_commit_hash(upgrade_args.commit_hash);
